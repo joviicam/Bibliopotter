ff3c5bc191eefe4e48cf9d971dd6f24c4e1df555
DiositoDiego <20213tn096@utez.edu.mx>
2023-04-20 23:50:22 -0600

    Creo que ya quedo los prestamos

--- src/screens/PrestamosCrudScreen.js
+++ src/screens/PrestamosCrudScreen.js
@@ -6,13 +6,14 @@ import { Button } from "react-native-elements";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { path } from "../utils/path";
 import colors from "../utils/colors";
+import Toast from "react-native-toast-message";
 
 export default function PrestamosCrudScreen({ mode }) {
   const route = useRoute();
   const prestamo = route.params;
-  const [selectedValue, setSelectedValue] = useState(null);
+  const [selectedValue, setSelectedValue] = useState(prestamo ? prestamo.idLibro : "");
   const [libros, setLibros] = useState([]);
-  const [nombrePersona, setNombrePersona] = useState("");
+  const [nombrePersona, setNombrePersona] = useState(prestamo ? prestamo.nombrePersona : "");
   const navigation = useNavigation();
 
   const fetchLibros = () => {
@@ -33,10 +34,127 @@ export default function PrestamosCrudScreen({ mode }) {
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  const updateLibro = (idLibro, prestamo) => {
+    const libro = libros.find(libro => libro.idLibro === selectedValue)
+    fetch(`${path}/libros/${idLibro}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        cantidad: prestamo ? parseInt(libro.cantidad) - 1 : parseInt(libro.cantidad) + 1,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
+  const areFieldsValidated = () => {
+    if (nombrePersona === "" || selectedValue === null) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = () => {
+    if(areFieldsValidated()){
+      const libro = libros.find(libro => libro.idLibro === selectedValue)
+      if(mode !== 'edit'){
+        if(libro.cantidad > 0){
+          fetch(`${path}/prestamos`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              nombrePersona: nombrePersona,
+              idLibro: selectedValue,
+            }),
+          })
+            .then((response) => {
+              console.log(response);
+              return response.json();
+            })
+            .then((data) => {
+              updateLibro(selectedValue, true);
+              Toast.show({
+                type: "success",
+                position: "bottom",
+                text1: "Prestamo creado",
+              });
+              navigation.goBack();
+            })
+        } else {
+          Toast.show({
+            type: "error",
+            position: "bottom",
+            text1: "Error, no hay libros disponibles",
+          })
+        }
+      } else {
+        fetch(`${path}/prestamos/${prestamo.idPrestamo}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nombrePersona: nombrePersona,
+            idLibro: selectedValue,
+          }),
+        })
+          .then((response) => {
+            console.log(response);
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            Toast.show({
+              type: "success",
+              position: "bottom",
+              text1: "Prestamo actualizado",
+            });
+            navigation.goBack();
+          })
+      }
+    }
+  }
+
+  const handleDelete = () => {
+    fetch(`${path}/prestamos/${prestamo.idPrestamo}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => { 
+        updateLibro(prestamo.idLibro, false);
+        Toast.show({
+          type: "success",
+          position: "bottom",
+          text1: "Libro regresado",
+        });
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   useEffect(() => {
-    if(mode === "edit" || mode === "view"){
+    if (mode === "edit" || mode === "view") {
       fetch(`${path}/prestamos/${prestamo.idPrestamo}`, {
         method: "GET",
         headers: {
@@ -49,7 +167,7 @@ export default function PrestamosCrudScreen({ mode }) {
         })
         .then((data) => {
           console.log(data);
-          fetchLibros()
+          fetchLibros();
           setSelectedValue(data.libro.idLibro);
           setNombrePersona(data.nombrePersona);
         })
@@ -64,13 +182,17 @@ export default function PrestamosCrudScreen({ mode }) {
   return (
     <View style={{ ...styles.container }}>
       <View style={{ ...styles.view }}>
-        <View style={{height: "70%"}}>
+        <View style={{ height: "70%" }}>
           <View>
             <View>
               <Text style={{ ...styles.text }}>Nombre del cliente:</Text>
             </View>
             <View>
-              <TextInput style={{ ...styles.input, ...styles.text }} value={nombrePersona} onChangeText={(text) => setNombrePersona(text)}/>
+              <TextInput
+                style={{ ...styles.input, ...styles.text }}
+                value={nombrePersona}
+                onChangeText={(text) => setNombrePersona(text)}
+              />
             </View>
           </View>
           <View style={{ marginTop: 45 }}>
@@ -82,27 +204,47 @@ export default function PrestamosCrudScreen({ mode }) {
                 <Picker
                   selectedValue={selectedValue}
                   onValueChange={(itemValue) => setSelectedValue(itemValue)}
+                  enabled={mode === "edit" || mode === "view" ? false : true}
                 >
-                  <Picker.Item label={"Selecciona un libro"} value={null}/>
+                  <Picker.Item
+                    label={
+                      mode === "edit" || mode === "view"
+                        ? libros[0].titulo
+                        : "Selecciona un libro"
+                    }
+                    value={
+                      mode === "edit" || mode === "view"
+                        ? libros[0].idLibro
+                        : null
+                    }
+                  />
                   {libros
-                    ? libros.map((libro, index) => (
-                        <Picker.Item label={libro.titulo} value={libro.idLibro} key={index}/>
-                      ))
+                    ? libros.map((libro, index) =>
+                        libro.cantidad > 0 ? (
+                          <Picker.Item
+                            label={libro.titulo}
+                            value={libro.idLibro}
+                            key={index}
+                          />
+                        ) : null
+                      )
                     : null}
                 </Picker>
               </View>
             </View>
           </View>
         </View>
-        <View style={{height: "30%"}}>
+        <View style={{ height: "30%" }}>
           <View style={{ width: "100%" }}>
-            <View style={{ marginTop: 45 }}>
-              <Button
-                //onPress={}
-                buttonStyle={{ ...styles.btnSave }}
-                title={"Guardar"}
-              />
-            </View>
+            {mode === "view" ? null : (
+              <View style={{ marginTop: 45 }}>
+                <Button
+                  onPress={handleSubmit}
+                  buttonStyle={{ ...styles.btnSave }}
+                  title={mode === "edit" ? "Guardar" : "Prestar"}
+                />
+              </View>
+            )}
             <View style={{ marginTop: 25, ...styles.containerBtn }}>
               <View>
                 <Button
@@ -113,13 +255,14 @@ export default function PrestamosCrudScreen({ mode }) {
                   title={"Atrás"}
                 />
               </View>
-              <View>
+              {mode === 'edit' ?
+                <View>
                 <Button
-                  //onPress={}
+                  onPress={handleDelete}
                   buttonStyle={{ ...styles.btnDelete }}
-                  title={"Eliminar"}
+                  title={"Devolver"}
                 />
-              </View>
+              </View>: null}
             </View>
           </View>
         </View>
@@ -137,7 +280,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: "10%",
     flex: 1,
-    backgroundColor: colors.fondo
+    backgroundColor: colors.fondo,
   },
   view: {
     width: "100%",
@@ -154,7 +297,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 12,
     padding: 10,
-    backgroundColor: colors.input
+    backgroundColor: colors.input,
   },
   picker: {
     marginTop: 15,
@@ -164,7 +307,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 12,
     padding: 10,
-    backgroundColor: colors.input
+    backgroundColor: colors.input,
   },
   btnSave: {
     backgroundColor: "#64C54B",
@@ -176,7 +319,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   btnCancel: {
-    width: "80%",
+    width: "100%",
     backgroundColor: "gray",
     alignItems: "center",
     justifyContent: "center",
@@ -185,12 +328,11 @@ const styles = StyleSheet.create({
     borderRadius: 12,
   },
   btnDelete: {
-    width: "80%",
+    width: "100%",
     backgroundColor: "#F00E0E",
     alignItems: "center",
     justifyContent: "center",
     alignContent: "center",
-    marginLeft: 20,
     height: 50,
     borderRadius: 12,
   },
@@ -198,8 +340,10 @@ const styles = StyleSheet.create({
     width: "100%",
     display: "flex",
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     alignContent: "center",
+    paddingLeft: 35,
+    paddingRight: 35,
   },
 });
